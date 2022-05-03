1029ba436869e165fb0ba37f1a307a97251681ec
Sonil Ejaz <sonil@MacBook-Pro-som-tillhor-Sonil.local>
2022-05-03 10:39:09 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import { Changelog } from './components/Changelog';
+
+import logs from './data/records.json';
+
+// const logs = [
+//   {
+//     name: 'Molly Marchall',
+//     message:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quos vitae eos, ex ipsum omnis, iste, nobis explicabo delectus provident molestiae. Repellendus unde minus quod nemo modi ullam id animi!',
+//     id: 1,
+//   },
+//   {
+//     name: 'Noah Simpson',
+//     message:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam aut numquam iste iusto incidunt neque, ducimus accusantium perspiciatis impedit aliquam quod deserunt suscipit, accusamus ipsum, quis ullam nesciunt facilis autem.',
+//     id: 2,
+//   },
+//   {
+//     name: 'Molly Marchall',
+//     message:
+//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni similique ratione explicabo nisi praesentium excepturi pariatur totam. Distinctio rem obcaecati ut corporis delectus. Dolorem enim earum deleniti eius, rem similique!',
+//     id: 3,
+//   },
+//   {
+//     name: 'Molly Marchall',
+//     message:
+//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni similique ratione explicabo nisi praesentium excepturi pariatur totam. Distinctio rem obcaecati ut corporis delectus. Dolorem enim earum deleniti eius, rem similique!',
+//     id: 4,
+//   },
+//   {
+//     name: 'Arabella Wang',
+//     message:
+//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni similique ratione explicabo nisi praesentium excepturi pariatur totam. Distinctio rem obcaecati ut corporis delectus. Dolorem enim earum deleniti eius, rem similique!',
+//     id: 5,
+//   },
+//   {
+//     name: 'Mini Skutt',
+//     message:
+//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni similique ratione explicabo nisi praesentium excepturi pariatur totam. Distinctio rem obcaecati ut corporis delectus. Dolorem enim earum deleniti eius, rem similique!',
+//     id: 6,
+//   },
+//   {
+//     name: 'Arabella Wang',
+//     message:
+//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni similique ratione explicabo nisi praesentium excepturi pariatur totam. Distinctio rem obcaecati ut corporis delectus. Dolorem enim earum deleniti eius, rem similique!',
+//     id: 7,
+//   },
+//   {
+//     name: 'Hasse Aro',
+//     message:
+//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni similique ratione explicabo nisi praesentium excepturi pariatur totam. Distinctio rem obcaecati ut corporis delectus. Dolorem enim earum deleniti eius, rem similique!',
+//     id: 8,
+//   },
+//   {
+//     name: 'Zoltan Beef',
+//     message:
+//       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni similique ratione explicabo nisi praesentium excepturi pariatur totam. Distinctio rem obcaecati ut corporis delectus. Dolorem enim earum deleniti eius, rem similique!',
+//     id: 9,
+//   },
+// ];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="Intro">
+        <h1>Changelog</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
+          repellat nulla pariatur fugit odit labore, laudantium consequuntur
+          magnam tempore distinctio minima quam dolores.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <Changelog logs={logs} />
     </div>
   );
 }
---
+++ src/components/ChangelogPreview.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+
+// styles && icons
+import styles from './ChangelogPreviw.module.css';
+import { BsPlayCircleFill } from 'react-icons/bs';
+import { BsXCircleFill } from 'react-icons/bs';
+
+export const ChangelogPreview = ({ logs }) => {
+  const [showFullActivity, setShowFullActivity] = useState(false);
+
+  // comment 4 and above not shown in log preview
+  const moreComments = () => {
+    // selecting the comments that's hidden
+    let result = logs
+      .slice(3)
+      .reduce((acc, o) => ((acc[o.name] = (acc[o.name] || 0) + 1), acc), {});
+    console.log('the if', Object.keys(result).length);
+    // return if there is above 4 comments
+    if (logs.length > 3) {
+      return (
+        <span>
+          +{logs.length - 3} more comments from{' '}
+          {Object.keys(result)[0].substring(
+            0,
+            Object.keys(result)[0].indexOf(' ')
+          )}
+          {Object.keys(result).length == 2 ? ' and' : ''}
+          {Object.keys(result).length > 2 ? ',' : ''}{' '}
+          {Object.keys(result).length > 1
+            ? Object.keys(result)[1].substring(
+                0,
+                Object.keys(result)[1].indexOf(' ')
+              )
+            : ''}{' '}
+          {Object.keys(result).length < 3
+            ? ''
+            : 'and ' + (Object.keys(result).length - 2) + ' others'}
+        </span>
+      );
+    }
+  };
+
+  console.log(showFullActivity);
+  return (
+    <div className={styles.changelog}>
+      {/* Render list of the 3 first changelogs */}
+      {logs &&
+        !showFullActivity &&
+        logs.slice(0, 3).map((logs) => (
+          <p key={logs.id}>
+            <span className={styles.name}>{logs.name}</span>:{' '}
+            {logs.message.length > 80
+              ? `${logs.message.substring(0, 80)}...`
+              : logs.message}
+          </p>
+        ))}
+      {/* Render full list of changelogs */}
+      {logs &&
+        showFullActivity &&
+        logs.map((logs) => (
+          <p key={logs.id}>
+            <span className={styles.name}>{logs.name}</span>: {logs.message}
+          </p>
+        ))}
+      <div className={styles.additional}>
+        {/* Info about additional comments  */}
+        {!showFullActivity && moreComments()}
+        {/* Buttons to show or hide full activity  */}
+        {!showFullActivity && (
+          <span
+            className={styles.fullActivity}
+            onClick={() => setShowFullActivity(true)}
+          >
+            View full activity log{' '}
+            <BsPlayCircleFill className={styles.viewIcon} />
+          </span>
+        )}
+        {showFullActivity && (
+          <span
+            className={styles.fullActivity}
+            onClick={() => setShowFullActivity(false)}
+          >
+            Hide full activity log <BsXCircleFill className={styles.viewIcon} />
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
